19bbd54aeb10d293f2b5089d55f8422707f463a3
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-25 23:51:19 -0500

    Add recursive solution for Fibonacci assignment

--- fibonacci.js
+++ fibonacci.js
@@ -18,18 +18,28 @@ function fibs(reps) {
   return fibsArray;
 }
 
-// console.log(fibs(-2));
-// console.log(fibs(0));
-// console.log(fibs(1));
-// console.log(fibs(8));
-// console.log(fibs(20));
+console.log(fibs(-2));
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(8));
+console.log(fibs(20));
 
 function fibsRec(reps) {
   if (reps <= 0) {
     return [];
+  } else if (reps === 1) {
+    return [0];
+  } else if (reps === 2) {
+    return [0, 1];
   }
 
-  return [reps].concat(fibsRec(--reps));
+  fibsArray = fibsRec(--reps);
+  fibsArray.push(fibsArray[reps - 2] + fibsArray[reps - 1]);
+  return fibsArray;
 }
 
-console.log(fibsRec(4));
+console.log(fibs(-2));
+console.log(fibs(0));
+console.log(fibs(1));
+console.log(fibs(8));
+console.log(fibs(20));
